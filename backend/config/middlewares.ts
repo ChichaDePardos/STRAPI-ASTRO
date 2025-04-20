1d05f0480b2c7971da5fdf73f521af58adfc4dd2
Chichadepardos <sbraithwaite@acasayc.org>
2025-04-20 12:08:01 -0500

    Agregado backend sin submódulo

---
+++ backend/config/middlewares.ts
@@ -0,0 +1,20 @@
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:4321'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: ['Content-Type', 'Authorization'],
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+]
